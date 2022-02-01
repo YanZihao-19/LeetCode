1b09d2c670fe8d55694e963eea62344d39e2672b
Yzh <1795342891@qq.com>
2022-02-01 09:33:17 +0800

    使用暴力解法

---
+++ 算法题/src/两数之和/Solution.java
@@ -0,0 +1,10 @@
+package 两数之和;
+
+public class Solution {
+  public static void main(String[] args) {
+    //
+    String s="122";
+    Integer integer=Integer.parseInt(s);
+    System.out.println(integer);
+  }
+}
